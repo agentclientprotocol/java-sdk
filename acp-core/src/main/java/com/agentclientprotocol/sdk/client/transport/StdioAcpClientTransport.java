099d8692e319489a9e49b3328574279fc411baab
Mark Pollack <mark.x.pollack@gmail.com>
2026-04-10 00:12:38 -0400

    Restore Duration import removed by PR #1

--- acp-core/src/main/java/com/agentclientprotocol/sdk/client/transport/StdioAcpClientTransport.java
+++ acp-core/src/main/java/com/agentclientprotocol/sdk/client/transport/StdioAcpClientTransport.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
